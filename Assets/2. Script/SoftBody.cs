5afc566e1ebc94f5252b9cfb33f16e53dcbc3c2e
kiwinim <123100214+kiwinim@users.noreply.github.com>
2026-03-27 11:29:38 +0900

    54
    fdghj

--- Assets/2. Script/SoftBody.cs
+++ Assets/2. Script/SoftBody.cs
@@ -10,6 +10,7 @@ public class SoftBody : MonoBehaviour
     #region Fields
     [SerializeField]
     public SpriteShapeController spriteShape;
+    public SpriteShapeController inspriteShape;
     [SerializeField]
     public Transform[] points;
     #endregion
@@ -39,20 +40,25 @@ public class SoftBody : MonoBehaviour
             try
             {
                 spriteShape.spline.SetPosition(i, (_vertex - _towardsCenter * _colliderRadius));
+                inspriteShape.spline.SetPosition(i, (_vertex - _towardsCenter * _colliderRadius));
             }
             catch
             {
                 Debug.Log("Spline points are too close to each other.. recalculate");
                 spriteShape.spline.SetPosition(i, (_vertex - _towardsCenter * (_colliderRadius + splineOffset)));
+                inspriteShape.spline.SetPosition(i, (_vertex - _towardsCenter * (_colliderRadius + splineOffset)));
             }
 
-            Vector2 _lt = spriteShape.spline.GetLeftTangent(i);
+            Vector2 _lt = inspriteShape.spline.GetLeftTangent(i);
+             _lt = spriteShape.spline.GetLeftTangent(i);
 
             Vector2 _newRt = Vector2.Perpendicular(_towardsCenter) * _lt.magnitude;
             Vector2 _newLt = Vector2.zero - (_newRt);
 
             spriteShape.spline.SetRightTangent(i, _newRt);
             spriteShape.spline.SetLeftTangent(i, _newLt);
+            inspriteShape.spline.SetRightTangent(i, _newRt);
+            inspriteShape.spline.SetLeftTangent(i, _newLt);
         }
     }
     #endregion
